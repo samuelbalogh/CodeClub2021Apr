0ce48b83e9d34b4618701b47c2edfb21b03156d0
Samuel Balogh <samuel.balogh@plentific.com>
2021-04-20 14:36:19 +0100

    work on fifth lesson

---
+++ 05.py
@@ -0,0 +1,115 @@
+my_cocktail_bar = [
+    'gin', 'tonic', 'lemon', 'lemon peel', 'lemon juice',
+    'water', 'whisky', 'angostura bitters',
+    'dry vermouth', 'gin',
+    'ice', 'sugar cube', 'sugar', 'orange',
+    'orange peel', 'vodka', 'grapefruit juice',
+    'drambuie', 'cranberry juice', 'orange juice', 'grapefruit juice'
+]
+
+# These are cocktail recipes:
+old_fashioned = ['whisky', 'angostura bitters', 'sugar', 'water', 'lemon peel', 'ice']
+dry_martini = ['dry vermouth', 'gin', 'ice']
+rusty_nail = ['drambuie', 'whisky', 'ice']
+espresso_martini = ['kahlua', 'vodka', 'espresso', 'sugar']
+rob_roy = ['angostura bitters', 'sweet vermouth', 'whisky', 'ice']
+margarita = ['cointreau', 'lime juice', 'tequila', 'ice']
+whisky_sour = ['lemon juice', 'bourbon', 'egg', 'sugar']
+
+
+cocktail_recipe_book = {
+    'Old Fashioned': old_fashioned,
+    'Dry Martini': dry_martini,
+    'Rusty Nail': rusty_nail,
+    'Espresso Martini': espresso_martini,
+    'Rob Roy': rob_roy,
+    'Margarita': margarita,
+    'Whisky Sour': whisky_sour
+}
+
+for item in my_cocktail_bar:
+    print(item)
+
+
+# can I make an old fashioned cocktail?
+i_can_make_it = True
+
+for ingredient in old_fashioned:
+    if ingredient not in my_cocktail_bar:
+        i_can_make_it = False
+
+print(i_can_make_it)
+
+
+# can I make a margarita cocktail?
+for ingredient in margarita:
+    if ingredient not in my_cocktail_bar:
+        i_can_make_it = False
+
+print(i_can_make_it)
+
+
+# ASIDE: strings and lists
+
+my_string = 'hello world!'
+# we can strip characters from a string
+my_string = my_string.strip('!')
+
+
+# we can join all elements of a list with the .join() method:
+food_at_home = ['bread', 'cucumber', 'curly kale', 'soba noodles', 'miso soup']
+food_at_home = ' '.join(food_at_home)
+
+# we can split a string into a list
+
+
+
+# opening a file
+# word counting
+# looping
+
+
+## WORKING WITH FILES
+
+# opening a file
+freq_words_file = open('hungarian.txt', 'r')
+
+common_words = []
+
+# reading a file, line by line
+for word in freq_words_file:
+    common_words.append(word.strip())
+
+# we have to close the file after we are done with it
+freq_words_file.close()
+
+
+# a better way of opening a file - this way, we don't have to close it,
+# as it will be closed automatically
+common_words = []
+with open('hungarian.txt', 'r') as freq_words_file:
+    for word in freq_words_file:
+        common_words.append(word.strip())
+
+
+# reading a file, line by line
+with open('kenyer_bor.txt', 'r') as text_file:
+    for line in text_file:
+        print(line)
+
+
+
+# counting words in a file, excluding common words
+word_count = {}
+with open('kenyer_bor.txt', 'r') as text_file:
+    for line in text_file:
+        for word in line.split():
+            word = word.lower()
+            for trailing_char in [' ', '.', '!', '?', ',', '"', '\'']:
+                word = word.strip(trailing_char)
+            if word in common_words:
+                continue
+            if word not in word_count:
+                word_count[word] = 1
+            else:
+                word_count[word] += 1
